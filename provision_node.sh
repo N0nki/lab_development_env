d459a98a935eabdcf9464410a800914799b2a118
N0nki <rgstr_land@icloud.com>
2019-02-16 14:46:00 +0900

    update python installation

--- provision_node.sh
+++ provision_node.sh
@@ -32,15 +32,10 @@ make
 sudo make install
 cd ~/
 
-echo "Python"
-echo 'python3 -m venv ~/myenv' >> setup_myenv.sh
-echo 'source ~/myenv/bin/activate' >> setup_myenv.sh
-echo 'pip install --upgrade pip' >> setup_myenv.sh
-echo 'pip install numpy pandas matplotlib networkx graphillion jupyter jupyte_contrib_nbextensions pynvim' >> setup_myenv.sh
-echo 'jupyter contrib nbextension install --user' >> setup_myenv.sh
-echo 'jupyter nbextensions_configurator enable --user' >> setup_myenv.sh
-echo 'deactivate' >> setup_myenv.sh
-echo 'alias myenv="source ~/myenv/bin/activate"' >>~/.bashrc
+# pip3 install --upgrade pip
+pip3 install numpy pandas matplotlib networkx graphillion ipython jupyter jupyter_contrib_nbextensions pynvim --user
+jupyter contrib nbextension install --user
+jupyter nbextensions_configurator enable --user
 
 echo "glpk"
 wget http://ftp.gnu.org/gnu/glpk/glpk-4.61.tar.gz
--- provision_node_simple.sh
+++ provision_node_simple.sh
@@ -15,12 +15,7 @@ echo "                  \__ \ | | | | | | |_) | |  __/";
 echo "                  |___/_|_| |_| |_| .__/|_|\___|";
 echo "                                  |_|           ";
 
-echo "Python"
-echo 'python3 -m venv ~/myenv' >> setup_myenv.sh
-echo 'source ~/myenv/bin/activate' >> setup_myenv.sh
-echo 'pip install --upgrade pip' >> setup_myenv.sh
-echo 'pip install numpy pandas matplotlib networkx graphillion jupyter jupyte_contrib_nbextensions' >> setup_myenv.sh
-echo 'jupyter contrib nbextension install --user' >> setup_myenv.sh
-echo 'jupyter nbextensions_configurator enable --user' >> setup_myenv.sh
-echo 'deactivate' >> setup_myenv.sh
-echo 'alias myenv="source ~/myenv/bin/activate"' >>~/.bashrc
+# pip3 install --upgrade pip
+pip3 install numpy pandas matplotlib networkx graphillion ipython jupyter jupyter_contrib_nbextensions --user
+jupyter contrib nbextension install --user
+jupyter nbextensions_configurator enable --user
